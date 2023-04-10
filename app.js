226c0f2fa59cb1e4b80c0ffa9d077ba700e165ec
KTL1216 <kentheolee@gmail.com>
2023-04-10 01:48:43 -0700

    update a9

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ import express from 'express';
 import cors from 'cors';
 
 import mongoose from 'mongoose';
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://leekent:<password>@ktl1216.zbt8gr6.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
-
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
+
 HelloController(app);
 UserController(app);
 TuitsController(app); 
